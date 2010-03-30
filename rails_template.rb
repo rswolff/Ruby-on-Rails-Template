aceec864da3c11c91e9982532a987da1e4297b71
Scott Wolff <scott.wolff@tuscan.ca>
2010-03-30 13:04:15 -0600

    removed duplicate container class

--- rails_template.rb
+++ rails_template.rb
@@ -1000,7 +1000,7 @@ end
 END
 
 file 'app/views/pages/home.html.erb', <<-END
-<div id="main" class="container">
+<div id="main">
   <h1>Welcome to #{current_app_name}</h1>
   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 </div>
